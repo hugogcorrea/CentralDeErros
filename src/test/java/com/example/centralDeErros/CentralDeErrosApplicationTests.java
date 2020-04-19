a1aea6a21494732f1de3ff79519af9ae86a33749
Hugo Gabriel Correa <hugogabrielcorrea@gmail.com>
2020-04-18 23:30:15 -0300

    first

---
+++ src/test/java/com/example/centralDeErros/CentralDeErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centralDeErros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

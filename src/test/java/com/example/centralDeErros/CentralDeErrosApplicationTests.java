e20c3f969db8c7f251477ffa625caa03b5e90e10
Robert Adam <r.neves1992@gmail.com>
2020-06-30 23:34:15 -0300

    Implementando novos testes

--- src/test/java/com/example/centralDeErros/CentralDeErrosApplicationTests.java
+++ src/test/java/com/example/centralDeErros/CentralDeErrosApplicationTests.java
@@ -2,22 +2,31 @@ package com.example.centralDeErros;
 
 import com.v1.CentralDeErros.exceptions.EmptyListException;
 import com.v1.CentralDeErros.exceptions.NotFoundException;
+import com.v1.CentralDeErros.models.DTOs.UserDTO;
+import com.v1.CentralDeErros.models.UserApplication;
 import com.v1.CentralDeErros.repositories.ErrorRepository;
 import com.v1.CentralDeErros.repositories.ServerRepository;
+import com.v1.CentralDeErros.repositories.UserRepository;
 import com.v1.CentralDeErros.services.ErrorService;
 import com.v1.CentralDeErros.services.ServerService;
+import com.v1.CentralDeErros.services.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import static org.mockito.Mockito.when;
-
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootConfiguration
 @SpringBootTest
 class CentralDeErrosApplicationTests {
+
+	@Mock
+	UserApplication userApplication;
 
 	@Mock
 	ErrorRepository errorRepository;
@@ -25,11 +34,17 @@ class CentralDeErrosApplicationTests {
 	@Mock
 	ServerRepository serverRepository;
 
+	@Mock
+	UserRepository userRepository;
+
 	@InjectMocks
 	ErrorService errorService;
 
 	@InjectMocks
 	ServerService serverService;
+
+	@InjectMocks
+	UserService userService;
 
 	@Test
 	void contextLoads() {
@@ -43,6 +58,17 @@ class CentralDeErrosApplicationTests {
 	@Test()
 	public void shouldReturnNotFoundException(){
 		Assertions.assertThrows(NotFoundException.class,() -> serverService.findAll(2));
+	}
+
+	@Test()
+	public void shouldReturnUsernameNotFoundException(){
+		Assertions.assertThrows(UsernameNotFoundException.class,() -> userService.findUser("Teste"));
+	}
+
+	@Test()
+	public void shouldReturnNullPointerException(){
+		UserDTO userDTO = new UserDTO();
+		Assertions.assertThrows(NullPointerException.class,() -> userService.saveUser(userDTO));
 	}
 
 }
